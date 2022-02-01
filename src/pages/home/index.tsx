737020a88e049381faba54d7248c017c68772ff7
Yury Regis <yury_regis@hotmail.com>
2022-02-01 17:21:36 -0300

    add flatlist with Heroes cards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,17 @@
-import { Container } from './style'
 import { Header } from './components'
 import homeLocale from '../../locale'
+import { FlatList } from 'react-native'
 import { getAllHeroes } from '../../services'
 import { getDeviceLanguage } from '../../utils'
 import { LoadingModal } from '../../components'
 import React, {useState, useEffect} from 'react'
+import { 
+    Text,
+    Avatar,
+    Container, 
+    RowContainer, 
+    CardContainer 
+} from './style'
 
 
 const deviceLanguage = getDeviceLanguage()
@@ -12,12 +19,16 @@ const locale = deviceLanguage==='pt_BR' ? homeLocale.ptBR : homeLocale.enUS
 
 export const Home: React.FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [offset, setOffset] = useState<number>(0)
+    const [count, setCount] = useState<number>(0)
     const [data, setData] = useState() 
    
     async function getHeroesHandler() {
         try {
-            const response = await getAllHeroes()
-            setData(_ => response)  
+            const { data: dataMarvel } = await getAllHeroes(offset)
+            setOffset(oldOffset => oldOffset + 4)
+            setCount(_ => dataMarvel.count)
+            setData(_ => dataMarvel.results)  
         } catch(error) {
 
         }
@@ -37,8 +48,22 @@ export const Home: React.FC = () => {
 
     return (
        <Container>
-           
+           {isLoading && <LoadingModal/>}
+
            <Header />
+           
+           <FlatList 
+            data={data}
+            keyExtractor={(item, _) => `${item.id}`}
+            renderItem={({item}) => (
+                <CardContainer>
+                    <RowContainer>
+                        <Avatar source={{uri: `${item.thumbnail.path}.${item.thumbnail.extension}` }} />
+                        <Text>{item.name}</Text>
+                    </RowContainer>
+                </CardContainer>
+            )}
+           />
        </Container>
     )
 }
--- src/pages/home/style.tsx
+++ src/pages/home/style.tsx
@@ -5,3 +5,33 @@ export const Container = styled.View`
     flex: 1;
     background: ${({theme}) => theme.colors.white};
 `
+
+export const CardContainer = styled.View`
+
+`
+
+export const RowContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+
+    border-bottom-width: 1px;
+    border-color: ${({theme}) => theme.colors.background};
+`
+
+
+
+export const Text = styled.Text`
+    color: ${({theme}) => theme.colors.text};
+
+    font-size: ${({theme}) => theme.metrics.text}px;
+    font-family: ${({theme}) => theme.font.regular};
+`
+
+export const Avatar = styled.Image`
+    width: ${({theme}) => theme.metrics.avatar}px;
+    height: ${({theme}) => theme.metrics.avatar}px;
+
+    border-radius: ${({theme}) => theme.metrics.avatar/2}px;
+
+    margin: ${({theme}) => theme.spacing.m}px;
+`
