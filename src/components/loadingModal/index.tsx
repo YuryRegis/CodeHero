0bb5f7a2fd81a87a11e50a9cb4be75041e6b0af1
Yury Regis <yury_regis@hotmail.com>
2022-02-01 17:19:25 -0300

    Creating loadign modal

--- src/components/loadingModal/index.tsx
+++ src/components/loadingModal/index.tsx
@@ -1,14 +1,16 @@
-import { Container } from './style'
 import React, {useContext} from 'react'
 import { ActivityIndicator } from 'react-native'
 import { ThemeContext } from 'styled-components'
+import { Container, ActivityContainer } from './style'
 
 
 const LoadingModal: React.FC = () => {
     const { colors } = useContext(ThemeContext)
     return (
         <Container>
-            <ActivityIndicator size="large" color={colors.white} />
+            <ActivityContainer>
+                <ActivityIndicator size="large" color={colors.white} />
+            </ActivityContainer>
         </Container>
     )
 }
--- src/components/loadingModal/style.tsx
+++ src/components/loadingModal/style.tsx
@@ -5,13 +5,19 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window')
 
 export const Container = styled.View`
-    display: absolute;
-
+    position: absolute;
+    
     width: ${width}px;
     height: ${height}px;
 
+    z-index: 1;
+
+    background:  rgba(212, 32, 38, .7);
+`
+
+export const ActivityContainer = styled.View`
+    flex: 1;
+
     justify-content: center;
     align-items: center;
-
-    background: rgba(212, 32, 38, .3);
 `
